b29cdec1d217e9df3864c692b1d5756f1f11f902
jimkon <kontzedakis_93@hotmail.com>
2018-03-07 22:01:21 +0200

    testing

--- src/ddpg/optimal_state_sequence.py
+++ src/ddpg/optimal_state_sequence.py
@@ -28,19 +28,37 @@ class State_sequence:
         if done:
             self._compare_sequences()
 
-    def goal_state(self, current_state):
+    def goal_state(self, state):
+        state = np.array(state).astype(float)
         if len(self._sequence) == 0:
             return np.zeros(self.state_size)
 
+        print('state', state)
         index, distance = self._flann.nn_index(state, 2)
+        print('indexes', index)
+        print('d', distance)
 
-        if distance[0] < self.STATE_REACHED_DISTANCE:
+        index = index[0]
+        distance = distance[0][0]
+
+        if distance > self.STATE_REACHED_DISTANCE:
+            print('result1', self._sequence[index[0]])
             return self._sequence[index[0]]
         else:
-            if index == len(self._sequence):
+            if index[0] == len(self._sequence) - 1:
+                print('result2', self._sequence[index[1]])
+
                 return self._sequence[index[1]]
             else:
+                print('result3', self._sequence[index[0] + 1])
+
                 return self._sequence[index[0] + 1]
+
+    def get_size(self):
+        return len(self._sequence)
+
+    def get_reward(self):
+        return self._total_reward
 
     def _reset_current_sequence(self):
         self._current_sequence = []
@@ -53,6 +71,6 @@ class State_sequence:
             self._total_reward = self._current_total_reward
             self._sequence = self._current_sequence
 
-            self._index = self._flann.build_index(self._sequence)
+            self._index = self._flann.build_index(np.copy(self._sequence).astype(float))
 
         self._reset_current_sequence()
